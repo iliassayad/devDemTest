9f7ed71febdd00e44f7726c9b02aeb7b0e210826
AYAD Iliass <ayadilyass282@gmail.com>
2025-06-12 13:54:41 +0100

    Initial commit: Structure du projet devDemTest Spring Boot

---
+++ src/main/java/net/ayad/devdemtest/DevDemTestApplication.java
@@ -0,0 +1,13 @@
+package net.ayad.devdemtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevDemTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevDemTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/ayad/devdemtest/DevDemTestApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ayad.devdemtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevDemTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
